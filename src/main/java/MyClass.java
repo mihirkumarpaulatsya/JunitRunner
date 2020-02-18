86b11a3dcea89bc409f32b6c02ab7e8c7c083856
Mihirkumar Paulatsya <mihirkumar.paulatsya@capgemini.com>
2020-02-18 07:56:36 +0100

    Committng the inital version

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,8 @@
+
+public class MyClass {
+
+	public int addition(int a,int b) {
+		return a+b;
+	}
+	
+}
---
+++ src/test/java/MyClassTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyClassTest {
+
+	//@Test
+	void testAddition() {
+		
+		int expected = 3;
+		
+		MyClass myClass = new MyClass();
+		int actual = myClass.addition(1, 2);
+		
+		assertEquals(expected, actual);
+		
+	}
+
+	@Test
+	void testAdditionAgain() {
+		
+		int expected = 4;
+		
+		MyClass myClass = new MyClass();
+		int actual = myClass.addition(1, 2);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	
+}
